e446505f79c15ae3b905020a3605d3ee0f6ea17b
David Worms <david@adaltas.com>
2024-06-21 00:45:32 +0200

    test: infrastructure

---
+++ eslint.config.js
@@ -0,0 +1,20 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+
+export default [
+  {
+    languageOptions: { globals: globals.browser },
+    rules: {
+      indent: [
+        "error",
+        2,
+        {
+          SwitchCase: 1,
+          flatTernaryExpressions: true,
+          offsetTernaryExpressions: false,
+        },
+      ],
+    },
+  },
+  pluginJs.configs.recommended,
+];
