b1f87832455bb7f01f3e5d6c20c8ecc4cf5d4167
David Worms <david@adaltas.com>
2024-06-25 21:37:14 +0200

    chore: eslint node globals

--- eslint.config.js
+++ eslint.config.js
@@ -3,7 +3,7 @@ import pluginJs from "@eslint/js";
 
 export default [
   {
-    languageOptions: { globals: globals.browser },
+    languageOptions: { globals: { ...globals.node } },
     rules: {
       indent: [
         "error",
