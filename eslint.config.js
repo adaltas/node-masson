3cb5d6079db3d25fbf60217cca0e3897aeb4f54f
David Worms <david@adaltas.com>
2024-07-19 22:58:59 +0200

    build: eslint prettier plugin

--- eslint.config.js
+++ eslint.config.js
@@ -1,20 +1,26 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
+import eslintConfigPrettier from "eslint-config-prettier";
 
 export default [
   {
     languageOptions: { globals: { ...globals.node } },
     rules: {
+      // See https://eslint.org/docs/latest/rules/indent
       indent: [
         "error",
         2,
         {
+          // (default: `0`) Enforces indentation level for case clauses in switch statements
           SwitchCase: 1,
-          flatTernaryExpressions: true,
-          offsetTernaryExpressions: false,
+          // `true` (`false` by default) requires no indentation for ternary expressions which are nested in other ternary expressions.
+          // flatTernaryExpressions: true,
+          // `true` (`false` by default) requires indentation for values of ternary expressions.
+          // offsetTernaryExpressions: false,
         },
       ],
     },
   },
   pluginJs.configs.recommended,
+  eslintConfigPrettier,
 ];
